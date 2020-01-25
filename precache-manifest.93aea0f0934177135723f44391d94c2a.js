7a091529248e57672602d788406142a4af412e9c
Alex Ling <22086776+alexling540@users.noreply.github.com>
2020-01-25 16:49:15 -0600

    Updates

---
+++ precache-manifest.93aea0f0934177135723f44391d94c2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "363a1cd87c66fd8297583bf327a6019a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba66f65ffcc2a79ac647",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5c7277979ccfe1224101",
+    "url": "/static/js/2.432899af.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.432899af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba66f65ffcc2a79ac647",
+    "url": "/static/js/main.2de8b239.chunk.js"
+  },
+  {
+    "revision": "bd31c327a3d97d5d7f44",
+    "url": "/static/js/runtime-main.87917586.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
