b94eebee8df06ae59dd57950984ab4280660ab3f
Alex Ling <22086776+alexling540@users.noreply.github.com>
2020-01-28 13:53:46 -0600

    react fullpage and reactstrap

--- src/App.js
+++ src/App.js
@@ -9,8 +9,42 @@ import Checkin from "./4_check_in/check_in"
 import Gate from "./5_gate/gate";
 import Takeoff from "./6_in_flight/takeoff";
 import Thanks from "./8_finish_page/thanks";
+import ReactFullpage from "@fullpage/react-fullpage";
 
 // #ffffff, #fff3f5, #ffdae0, #ffc0cb, #ffa7b6, #ff8da1, #ff748c, #ff5b77
+
+const Fullpage = () => (
+  <ReactFullpage
+    //fullpage options
+    licenseKey = {'YOUR_KEY_HERE'}
+    scrollingSpeed = {1000} /* Options here */
+    render={({ state, fullpageApi }) => {
+      return (
+        <ReactFullpage.Wrapper>
+          <div className={'section'}>
+            <FlightInfo date={'1/25/2020'} departure={'10:00am'} boarding={'9:30am'} gate={'13B'} arrival={'3:00pm'}
+                        baggage={'41'}/>
+            <button onClick={() => fullpageApi.moveSectionDown()}>
+              Click me to move down
+            </button>
+          </div>
+          <div className="section">
+            <Checkin/>
+          </div>
+          <div className="section">
+            <Gate/>
+          </div>
+          <div className={'section'}>
+            <Takeoff/>
+          </div>
+          <div className={'section'}>
+            <Thanks cityname={'Rio de Janeiro'}/>
+          </div>
+        </ReactFullpage.Wrapper>
+      );
+    }}
+  />
+);
 
 function App() {
     return (
@@ -18,13 +52,8 @@ function App() {
             <Sidebar/>
             <Infobar departure={'10:00am'} flightNumber={"DL31"}/>
             <div id={'main'}>
+                <Fullpage />
                 <Landing/>
-                <FlightInfo date={'1/25/2020'} departure={'10:00am'} boarding={'9:30am'} gate={'13B'} arrival={'3:00pm'}
-                            baggage={'41'}/>
-                <Checkin/>
-                <Gate/>
-                <Takeoff/>
-                <Thanks cityname={'Rio de Janeiro'}/>
             </div>
         </div>
     );
